94e04632e1cdb53067a0ce3f296448ae9c10aef3
zhucy1001-cloud <zhucy1001@gmail.com>
2026-08-27 16:51:41 +0800

    Update BlogPostCard.js

--- themes/hexo/components/BlogPostCard.js
+++ themes/hexo/components/BlogPostCard.js
@@ -7,30 +7,17 @@ import { BlogPostCardInfo } from './BlogPostCardInfo'
 const BlogPostCard = ({ index, post, showSummary, siteInfo }) => {
   const showPreview =
     siteConfig('HEXO_POST_LIST_PREVIEW', null, CONFIG) && post.blockMap
-
-  // 🌟 核心修改：如果文章没有手动设置封面，自动分配 LoremFlickr 的 F1 真实相片
-  if (post && !post.pageCoverThumbnail) {
-    // 巧妙利用 post.id 和 index 计算出一个固定的随机数种子 (lockId)
-    const lockId = post.id
-      ? post.id.charCodeAt(post.id.length - 1) + post.id.charCodeAt(post.id.length - 2) + index
-      : Math.floor(Math.random() * 1000)
-
-    // 调用 LoremFlickr 接口，关键词锁定 formula1, racing, f1
-    post.pageCoverThumbnail = `https://loremflickr.com/1200/800/formula1,racing,f1?lock=${lockId}`
-  } else if (
+  if (
     post &&
     !post.pageCoverThumbnail &&
     siteConfig('HEXO_POST_LIST_COVER_DEFAULT', null, CONFIG)
   ) {
-    // 兼容原有逻辑（不过上面的 api 拦截通常会先执行）
     post.pageCoverThumbnail = siteInfo?.pageCover
   }
-
   const showPageCover =
     siteConfig('HEXO_POST_LIST_COVER', null, CONFIG) &&
     post?.pageCoverThumbnail &&
     !showPreview
-
   //   const delay = (index % 2) * 200
 
   return (
