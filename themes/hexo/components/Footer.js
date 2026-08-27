ab14cd45e7345228a194b3d506a5e3d47138e354
zhucy1001-cloud <zhucy1001@gmail.com>
2026-08-28 01:23:43 +0800

    Update Footer.js

--- themes/hexo/components/Footer.js
+++ themes/hexo/components/Footer.js
@@ -4,24 +4,23 @@ import PoweredBy from '@/components/PoweredBy'
 import { siteConfig } from '@/lib/config'
 
 const Footer = ({ title }) => {
-  const d = new Date()
-  const currentYear = d.getFullYear()
-  const since = siteConfig('SINCE')
-  const copyrightDate =
-    parseInt(since) < currentYear ? since + '-' + currentYear : currentYear
-
   return (
-    <footer className='relative z-10 dark:bg-black flex-shrink-0 bg-hexo-light-gray justify-center text-center m-auto w-full leading-6  text-gray-600 dark:text-gray-100 text-sm p-6'>
-      {/* <DarkModeButton/> */}
-      <i className='fas fa-copyright' /> {`${copyrightDate}`}
-      <span>
-        <i className='mx-1 animate-pulse fas fa-heart' />
+    <footer className='relative z-10 dark:bg-black flex-shrink-0 bg-hexo-light-gray justify-center text-center m-auto w-full leading-6 text-gray-600 dark:text-gray-100 text-sm p-6'>
+      
+      {/* 美化后的版权与作者信息 */}
+      <div className='flex items-center justify-center font-mono tracking-wider text-gray-500 dark:text-gray-400'>
+        <i className='fas fa-copyright mr-2' /> 
+        <span>Since 2026</span>
+        <i className='mx-3 animate-pulse fas fa-heart text-red-500' />
         <a
           href={siteConfig('LINK')}
-          className='underline font-bold  dark:text-gray-300 '>
+          className='font-bold text-gray-700 dark:text-gray-200 hover:text-indigo-500 dark:hover:text-indigo-400 transition-colors duration-300'>
           {siteConfig('AUTHOR')}
         </a>
-        .<br />
+      </div>
+
+      {/* 其他站点信息 */}
+      <div className='mt-1'>
         <BeiAnSite />
         <BeiAnGongAn />
         <span className='hidden busuanzi_container_site_pv'>
@@ -33,14 +32,13 @@ const Footer = ({ title }) => {
           <span className='px-1 busuanzi_value_site_uv'> </span>
         </span>
         
-        {/* 这里修改为了固定文本 */}
-        <h1 className='text-xs pt-4 text-light-400 dark:text-gray-400'>
+        {/* 固定文本 */}
+        <h1 className='text-xs pt-4 text-light-400 dark:text-gray-500 tracking-widest'>
           | Evan Space |
         </h1>
         
-        <PoweredBy className='justify-center' />
-      </span>
-      <br />
+        <PoweredBy className='justify-center mt-2' />
+      </div>
     </footer>
   )
 }
