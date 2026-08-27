0361e536c9fe15b92328bff78c05bd864b816f99
zhucy1001-cloud <zhucy1001@gmail.com>
2026-08-28 01:33:22 +0800

    Update Footer.js

--- themes/hexo/components/Footer.js
+++ themes/hexo/components/Footer.js
@@ -7,14 +7,14 @@ const Footer = ({ title }) => {
   return (
     <footer className='relative z-10 dark:bg-black flex-shrink-0 bg-hexo-light-gray justify-center text-center m-auto w-full leading-6 text-gray-600 dark:text-gray-100 text-sm p-6'>
       
-      {/* 使用 flex-col 和 space-y-2 让内部所有子元素的上下间距完全保持一致 */}
-      <div className='flex flex-col items-center justify-center space-y-2'>
+      {/* 移除了 space-y-2，所有行将紧密贴合 */}
+      <div className='flex flex-col items-center justify-center'>
         
         {/* 第 1 行: 版权与作者信息 */}
-        <div className='flex items-center justify-center font-mono tracking-wider text-gray-500 dark:text-gray-400'>
-          <i className='fas fa-copyright mr-2' /> 
+        <div className='font-mono tracking-wider text-gray-500 dark:text-gray-400'>
+          <i className='fas fa-copyright mr-1' /> 
           <span>Since 2026</span>
-          <i className='mx-3 animate-pulse fas fa-heart text-red-500' />
+          <i className='mx-2 animate-pulse fas fa-heart text-red-500' />
           <a
             href={siteConfig('LINK')}
             className='font-bold text-gray-700 dark:text-gray-200 hover:text-indigo-500 dark:hover:text-indigo-400 transition-colors duration-300'>
@@ -22,8 +22,8 @@ const Footer = ({ title }) => {
           </a>
         </div>
 
-        {/* 第 2 行: 访问量统计等信息 */}
-        <div className='flex items-center justify-center text-gray-500 dark:text-gray-400'>
+        {/* 第 2 行: 访问量统计 */}
+        <div className='text-gray-500 dark:text-gray-400'>
           <BeiAnSite />
           <BeiAnGongAn />
           <span className='hidden busuanzi_container_site_pv'>
@@ -36,12 +36,12 @@ const Footer = ({ title }) => {
           </span>
         </div>
         
-        {/* 第 3 行: 固定文本 */}
-        <h1 className='text-xs text-light-400 dark:text-gray-500 tracking-widest'>
+        {/* 第 3 行: 固定文本 (去除了之前遗留的 pt-4 属性) */}
+        <div className='text-xs text-light-400 dark:text-gray-500 tracking-widest'>
           | Evan Space |
-        </h1>
+        </div>
         
-        {/* 第 4 行: Powered By (去除了之前单独附加的 mt-2，由外层统一控制) */}
+        {/* 第 4 行: Powered By */}
         <PoweredBy className='justify-center' />
         
       </div>
